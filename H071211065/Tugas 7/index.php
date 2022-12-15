d47876390be2b2ab5d72c4a0aea1c2c0835a8987
Asehpryanto <asehpryantorari@gmail.com>
2022-12-15 23:27:03 +0800

    Praktikum-7 Asehpryanto

---
+++ H071211065/Tugas 7/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// class Fruit {
+//     // Attribut/Properti
+//     public $name;
+//     public $color;
+//     public $price;
+
+
+//     function_construct($name, $color, $price = 3000) {
+//         $this->name = $name;
+//         $this->color = $color;
+//         $this->price = $price;
+//     }
+
+//     // Function/Method
+//     function getName($name) {
+//         return $this->name;
+//     } function getColor($color) {
+//         return $this->color;
+//     } function getPrice() {
+//         return $this->price;
+//     }
+
+// }
+
+// $fruit = new fruit();
+// var_dump($fruit);
+
+?>
